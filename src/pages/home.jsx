98fb905aaa4402996cf9c497b0415390d1795805
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-04-15 12:32:41 +0200

    created home structure

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
 
 function home() {
-  return (
-    <div>home</div>
-  )
+    const urlParams = new URLSearchParams(window.location.search);
+    const role = urlParams.get("role");
+
+    const renderHome = () => {
+
+        switch(role) {
+
+            case "employee":
+                renderEmployee();
+                break;
+
+            case "technical":
+                renderTechnical();
+                break;
+            
+            case "manager":
+                renderManager();
+                break;
+            
+            case "director":
+                renderDirector();
+        }
+    }
+
+
+    const renderTechnical = () => {
+        return (
+            <>
+
+            </>
+        )
+    }
+
+
+    const renderEmployee = () => {
+        return (
+            <>
+            
+            </>
+        )
+    }
+    
+    const renderManager = () => {
+        return (
+            <>
+            
+            </>
+        )
+    }
+    
+    const renderDirector = () => {
+        return (
+            <>
+            
+            </>
+        )
+    }
+
+
+
+    return <div>
+        {renderHome()}
+    </div>;
 }
 
-export default home
+export default home;

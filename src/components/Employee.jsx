8a0ae2ec260037df86c896fb923c3e3eb3ea1066
Vladdddy <vlad00anonimous00@gmail.com>
2025-04-15 15:04:53 +0200

    Employee page initialization

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-
+import React from "react";
 
 function Employee() {
-  return (
-    <div>Employee</div>
-  )
+    const url = "http://its.digitalminds.cloud/Dipendenti.json";
+
+    const dataRequest = async () => {
+        try {
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error("errore: " + response.status);
+            }
+
+            const json = response.json();
+
+            console.log(json);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    dataRequest();
+
+    return <div>Employee</div>;
 }
 
-export default Employee
+export default Employee;

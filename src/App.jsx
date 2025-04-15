fcace3e4a60d7655b90bfec8b383fe7c810fcb62
Vladdddy <vlad00anonimous00@gmail.com>
2025-04-15 14:46:44 +0200

    Restyling of the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
     return (
         <>
-        <Navbar/>
-        <Home/>
+            <Navbar />
+            <Home />
         </>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/navbar.css";
 
 function Navbar() {
     const handleClick = (type) => {
@@ -9,62 +10,40 @@ function Navbar() {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    Navbar
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
+        <nav>
+            <a className="navbar-brand" href="#">
+                <h1>Website</h1>
+            </a>
+
+            <div className="links">
+                <a
+                    onClick={() => handleClick("employee")}
+                    className="nav-link active"
+                    aria-current="page"
                 >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a
-                                onClick={() => handleClick("employee")}
-                                className="nav-link active"
-                                aria-current="page"
-                            >
-                                Employee
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                id="manager"
-                                onClick={() => handleClick("manager")}
-                            >
-                                Manager
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                id="director"
-                                onClick={() => handleClick("director")}
-                            >
-                                Director
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                id="legend"
-                                onClick={() => handleClick("legend")}
-                            >
-                                Legend
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                    Employee
+                </a>
+                <a
+                    className="nav-link"
+                    id="manager"
+                    onClick={() => handleClick("manager")}
+                >
+                    Manager
+                </a>
+                <a
+                    className="nav-link"
+                    id="director"
+                    onClick={() => handleClick("director")}
+                >
+                    Director
+                </a>
+                <a
+                    className="nav-link"
+                    id="legend"
+                    onClick={() => handleClick("legend")}
+                >
+                    Legend
+                </a>
             </div>
         </nav>
     );

dca6e7667900001ee8c781dd8c1a7ce04c38e4ed
Vladdddy <vlad00anonimous00@gmail.com>
2025-04-15 12:35:36 +0200

    merge main

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 function home() {
-    return <div>home</div>;
+    const urlParams = new URLSearchParams(window.location.search);
+    const role = urlParams.get("role");
+
+    const renderHome = () => {
+        switch (role) {
+            case "employee":
+                renderEmployee();
+                break;
+
+            case "technical":
+                renderTechnical();
+                break;
+
+            case "manager":
+                renderManager();
+                break;
+
+            case "director":
+                renderDirector();
+        }
+    };
+
+    const renderTechnical = () => {
+        return <></>;
+    };
+
+    const renderEmployee = () => {
+        return <></>;
+    };
+
+    const renderManager = () => {
+        return <></>;
+    };
+
+    const renderDirector = () => {
+        return <></>;
+    };
+
+    return <div>{renderHome()}</div>;
 }
 
 export default home;
